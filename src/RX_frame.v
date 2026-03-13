a3f0bba7edbb257295dcd4674b00c68d903bf5b1
Selyss <99344963+Selyss@users.noreply.github.com>
2026-03-13 10:56:13 -0400

    states

--- src/RX_frame.v
+++ src/RX_frame.v
@@ -12,4 +12,20 @@ module RX_frame (
     output reg [1:0] orx_status
 
 );
+
+  localparam IDLE = 3'b000;
+  localparam PREAMBLE = 3'b001;
+  localparam MAC = 3'b010;
+  localparam ETHTYPE = 3'b011;
+  localparam PAYOUT = 3'b100;
+  localparam FCS = 3'b101;
+  localparam DONE = 3'b110;
+
+  localparam STATUS_IDLE = 2'b00;
+  localparam STATUS_PROCESSING = 2'b01;
+  localparam STATUS_BYTE_VALID = 2'b10;
+  localparam STATUS_ERROR = 2'b11;
+
+
 endmodule
+
